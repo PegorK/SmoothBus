0c454bc38808ac735614a67131ad9ebfe7decbc1
Pegor <devpegor@gmail.com>
2025-02-23 11:11:47 -0800

    V2 Updates

--- src/README.md
+++ src/README.md
@@ -9,9 +9,5 @@ This folder contains the code that gets slapped onto the ATTiny85 chip using the
 
 ### Flashing
 
-* I used an Arduino Mega2560 as an ISP to flash the ATTiny85 with the programming jig. [Here](https://www.instructables.com/How-to-Burn-ATTiny85-Using-Arduino-Mega/) is a decent write up.
-    * Flashed the ATTiny85 with the clock set to 16 Mhz, anything less with introduce more servo jitter.
-
-## Things to improve
-* Discard readings that are dramatically different than previous reading. Could be based on percent difference.
-* Low fuel warning indicator logic could be improved to not trigger on every time it has a reading in that range. Instead turn the warning lights on if there has been continuous readings in that range (maybe something along the lines of if 20 readings) same with turning them off. This could help reduce flashing warning lights when you take a turn.
+* I used an Arduino Mega2560 as an ISP to flash the ATTiny85. [Here](https://www.instructables.com/How-to-Burn-ATTiny85-Using-Arduino-Mega/) is a decent write up.
+    * Flashed the ATTiny85 with the clock set to 16 Mhz, anything less will introduce more servo jitter.
--- src/SmoothBus/SmoothBus.ino
+++ src/SmoothBus/SmoothBus.ino
@@ -1,31 +1,54 @@
 //==========================================
-// Title:  SmoothBus.ino
-// Author: Pegor Karoglanian
-// Date:   11/1/24
-// Notes:  Code for SmoothBus fuel gauge conversion for VW Bus.
+// Title:   SmoothBus.ino
+// Author:  Pegor Karoglanian
+// Date:    2/22/25
+// Notes:   Code for SmoothBus fuel gauge conversion for VW Bus.
+// Version: 2.0
 //==========================================
 
-#include <ATTinyCore.h>
 #include <Arduino.h>
 #include <Servo.h>
 
-#ifdef DEBUG
-  #define SERVO_PIN 9
-  #define LOW_FUEL_LED 2
+#define CLASSIC_LINE_8115800400_SENDER 1
+
+#define SERVO_PIN             0 // (PB0)
+#define LOW_FUEL_LED          1 // (PB1)(D1)
+#define SENDER_PIN            1 // analog pin (PB2)(A1)
+#define WDT_PIN               3 // watchdog time reset pin (PB3)(D3)
+#define WDT_PW_US             2 // STWD100YNYWY3F asks at least 1us pulse width.
+#define WINDOW_SIZE           70 // window for moving average.
+#define READ_DELAY_MS         200 // fuel read frequency
+#define SERVO_STEP_DELAY_MS   15 // delay between steps in servo moves for smoother motion
+#define INPUT_RESISTANCE      4 // resistance difference measured at sender and dash.
+#define R1                    50 // resistor for voltage divider
+#define VIN                   5
+#define VREF                  5
+
+#if CLASSIC_LINE_8115800400_SENDER
+// these resistance values were found by testing my sender
+// using a bucket with markings at the corresponding fuel 
+// level heights (35mm = R, 75mm = 1/4 tank, 130mm = 1/2 tank, 200mm = Full)
+  #define IN_FULL     11
+  #define IN_HALF     37
+  #define IN_QUARTER  58
+  #define IN_R        77
+  #define IN_EMPTY    89
 #else
-  #define SERVO_PIN 0
-  #define LOW_FUEL_LED 1
+// these resistance values are based on info from here:
+// https://www.thesamba.com/vw/forum/viewtopic.php?t=693930
+  #define IN_FULL     10
+  #define IN_HALF     21
+  #define IN_QUARTER  33
+  #define IN_R        45
+  #define IN_EMPTY    79
 #endif
 
-#define WINDOW_SIZE 35 // window for moving average
-#define SENDER_PIN 1 // analog pin
-#define R1 101 // resistor for voltage divider
-#define VIN 5
-#define VREF 5
-#define SERVO_STEP_DELAY 15 // delay between steps in servo moves for smoother motion
-#define READ_DELAY_MS 200 // fuel read frequency
-#define MIN_RAW 10 // fuel sender tabs should be bent so that at FULL its 10 ohms
-#define MAX_RAW 79 // fuel sender tabs should be bent so that at EMPTY its 79 ohms
+// servo position values that matched my setup
+#define OUT_FULL      28
+#define OUT_HALF      53
+#define OUT_QUARTER   74
+#define OUT_R         88
+#define OUT_EMPTY     102
 
 class SmoothBus {
   private:
@@ -33,141 +56,106 @@ class SmoothBus {
     int servo_pin;
     int sender_pin;
     int low_fuel_indicator_pin;
-
+    int wdt_pin;
     int raw = 0;
-    // resistance values based on info from here:
-    // https://www.thesamba.com/vw/forum/viewtopic.php?t=693930
-    int in_full = 10;
-    int in_half = 21;
-    int in_quarter = 33;
-    int in_r = 45;
-    int in_empty = 79;
 
-    // servo values that matched my setup
-    int out_full = 28;
-    int out_half = 53;
-    int out_quarter = 71;
-    int out_r = 81;
-    int out_empty = 98;
-
-
-    int servo_pos = out_empty;
+    int servo_pos = OUT_EMPTY;
+    int sender_pos = 0;
     int index = 0;
     bool filled = false;
     float avg_arr[WINDOW_SIZE];
     float total = 0;
   
   public:
-    SmoothBus(int servo_pin, int sender_pin, int low_fuel_indicator_pin) {
+    SmoothBus(int servo_pin, int sender_pin, int low_fuel_indicator_pin, int wdt_pin) {
       this->servo_pin = servo_pin;
       this->sender_pin = sender_pin;
       this->low_fuel_indicator_pin = low_fuel_indicator_pin;
+      this->wdt_pin = wdt_pin;
 
       init();
     }
 
     void init() {
+      clearWdt();
       pinMode(this->low_fuel_indicator_pin, OUTPUT);
+      pinMode(this->wdt_pin, OUTPUT);
       servo.attach(this->servo_pin);
-      servoBootSequence();
+      // only do boot sequence on power up NOT on resets. (WDT)
+      if(bit_is_set(MCUSR, PORF)) {
+        MCUSR = 0;
+        servoBootSequence();
+      }
     }
 
+    // sliding boot sequence going from E to Full and back.
     void servoBootSequence() {
-      servo.write(this->out_empty);
-      delay(1000);
-      for (int pos = this->out_empty; pos >= this->out_full; pos -= 1) { // goes from out_empty degrees to out_full degrees
+      clearWdt();
+      servo.write(OUT_EMPTY);
+      delay(500);
+      clearWdt();
+      delay(500);
+      clearWdt();
+      for (int pos = OUT_EMPTY; pos >= OUT_FULL; pos -= 1) {
         servo.write(pos);
         delay(13);
+        clearWdt();
       }
       delay(500);
-      for (int pos = this->out_full; pos <= this->out_empty; pos += 1) { // goes from out_full degrees to out_empty degrees
+      for (int pos = OUT_FULL; pos <= OUT_EMPTY; pos += 1) {
         servo.write(pos);
         delay(13);
+        clearWdt();
       }
       delay(750);
+      clearWdt();
     }
 
     void readFuelSender() {
       this->raw = analogRead(this->sender_pin);
-#ifdef DEBUG
-  Serial.print("Analog read: ");
-  Serial.println(this->raw);
-#endif
-      if(this->raw) {
-        float Vout = (raw/1023.0) * VREF;
-        float fuel_sender = R1 * (Vout/(VIN - Vout));
-        if (fuel_sender < MIN_RAW) {
-          fuel_sender = MIN_RAW;
-        } else if (fuel_sender > MAX_RAW) {
-          fuel_sender = MAX_RAW;
+  
+      if (this->raw) {
+        float v_out = (this->raw/1023.0) * VREF;
+        float fuel_sender = R1 * (v_out/(VIN - v_out));
+        fuel_sender -= INPUT_RESISTANCE;
+
+        if (fuel_sender < IN_FULL) {
+          fuel_sender = IN_FULL;
+        } else if (fuel_sender > IN_EMPTY) {
+          fuel_sender = IN_EMPTY;
         }
-        // possible TODO - drop values that are a certain percentage over the last
-        // but keep track of back to back repetition for scenarios like filling up tank
+
         int current_avg = round(runningAverage(fuel_sender));
-#ifdef DEBUG
-  Serial.print("Current Avg: ");
-  Serial.println(current_avg);
-#endif
 
-        // map to ranges based on fuel sender calibration data from above forum.
-        int sender_pos = 0;
-        int out_min = 0;
-        int out_max = 0;
-        int in_min = 0;
-        int in_max = 0;
-
-        digitalWrite(this->low_fuel_indicator_pin, LOW);
-        if (current_avg >= this->in_full && current_avg <= this->in_half) { // 1/2 to FULL
-          out_min = this->out_full;
-          out_max = this->out_half;
-          in_min = this->in_full;
-          in_max = this->in_half;
-#ifdef DEBUG
-  Serial.println("1/2 to FULL");
-#endif
-        } else if (current_avg >= (this->in_half-1) && current_avg <= this->in_quarter) { //  1/4 to 1/2
-          out_min = this->out_half;
-          out_max = this->out_quarter;
-          in_min = this->in_half;
-          in_max = this->in_quarter;
-#ifdef DEBUG
-  Serial.println("1/4 to 1/2");
-#endif
-        } else if (current_avg >= (this->in_quarter-1) && current_avg <= this->in_r) { // R to 1/4
-          out_min = this->out_quarter;
-          out_max = this->out_r;
-          in_min = this->in_quarter;
-          in_max = this->in_r;
-#ifdef DEBUG
-  Serial.println("R to 1/4");
-#endif
+        if (current_avg >= IN_FULL && current_avg <= IN_HALF) { // 1/2 to FULL
+          this->sender_pos = mapSenderPos(current_avg, OUT_FULL, OUT_HALF, IN_FULL, IN_HALF);
+        } else if (current_avg > (IN_HALF) && current_avg <= IN_QUARTER) { //  1/4 to 1/2
+          this->sender_pos = mapSenderPos(current_avg, OUT_HALF, OUT_QUARTER, IN_HALF, IN_QUARTER);
+        } else if (current_avg > (IN_QUARTER) && current_avg <= IN_R) { // R to 1/4
+          this->sender_pos = mapSenderPos(current_avg, OUT_QUARTER, OUT_R, IN_QUARTER, IN_R);
         } else { // E to R
-          out_min = this->out_r;
-          out_max = this->out_empty;
-          in_min = this->in_r;
-          in_max = this->in_empty;
-          digitalWrite(this->low_fuel_indicator_pin, HIGH);
-#ifdef DEBUG
-  Serial.println("E to R");
-#endif
-        }
-
-        sender_pos = out_min + (out_max - out_min) * (current_avg - in_min) / (in_max - in_min);
-        if (sender_pos != this->servo_pos) {
-          setServoPos(sender_pos);
+          this->sender_pos = mapSenderPos(current_avg, OUT_R, OUT_EMPTY, IN_R, IN_EMPTY);
         }
       }
     }
 
-    void setServoPos(int pos) {
+    // map to ranges based on fuel sender calibration data from above forum.
+    int mapSenderPos(int current_avg, int out_min, int out_max, int in_min, int in_max) {
+       return out_min + (out_max - out_min) * (current_avg - in_min) / (in_max - in_min);
+    }
+
+    void updateServoPos() {
       // go in steps instead of shoot to position
-      while (this->servo_pos != pos) {
-        if (pos > this->servo_pos) {
-          servo.write(this->servo_pos++);
-        } else if (pos < this->servo_pos) {
-          servo.write(this->servo_pos--);
-        }
-        delay(SERVO_STEP_DELAY);
+      if (this->sender_pos > this->servo_pos) {
+        servo.write(this->servo_pos++);
+      } else if (this->sender_pos < this->servo_pos) {
+        servo.write(this->servo_pos--);
+      }
+
+      if (this->servo_pos >= OUT_R ) {
+        digitalWrite(this->low_fuel_indicator_pin, HIGH);
+      } else if (this->servo_pos < OUT_R) {
+        digitalWrite(this->low_fuel_indicator_pin, LOW);
       }
     }
 
@@ -176,36 +164,44 @@ class SmoothBus {
           this->total -= this->avg_arr[this->index];
       }
 
-      // Replace the oldest value with the new value
       this->avg_arr[this->index] = resistance;
-      this->total += resistance;  // Add the new value to the total
-
-      // Move the index for the next new value
+      this->total += resistance;
       this->index = (this->index + 1) % WINDOW_SIZE;
 
-      // Check if the window has been filled at least once
       if (this->index == 0) {
           this->filled = true;
       }
 
-      // Calculate and return the average
       float running_average = this->total / (this->filled ? WINDOW_SIZE : this->index);
       return running_average;
     }
+
+    void clearWdt() {
+      digitalWrite(this->wdt_pin, HIGH);
+      delayMicroseconds(WDT_PW_US);
+      digitalWrite(this->wdt_pin, LOW);
+    }
 };
 
-
-
 void setup() {
-#ifdef DEBUG
-  Serial.begin(9600);
-#endif
+  analogReference(DEFAULT);
 }
 
 void loop() {
-  SmoothBus smooth_bus(SERVO_PIN, SENDER_PIN, LOW_FUEL_LED);
+  SmoothBus smooth_bus(SERVO_PIN, SENDER_PIN, LOW_FUEL_LED, WDT_PIN);
+  unsigned long last_read = millis();
+  unsigned long last_servo_update = millis();
   while(1) {
-    smooth_bus.readFuelSender();
-    delay(READ_DELAY_MS);
+    if ((millis() - last_read) >= READ_DELAY_MS) {
+      smooth_bus.readFuelSender();
+      last_read = millis();
+    }
+
+    if ((millis() - last_servo_update) >= SERVO_STEP_DELAY_MS) {
+      smooth_bus.updateServoPos();
+      last_servo_update = millis();
+    }
+
+    smooth_bus.clearWdt();
   }
 }
